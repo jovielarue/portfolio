976e99ebb07591d2e2f7e6e907631ddadea825d8
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-05-17 16:35:38 -0600

    added ability to home page

--- src/components/Abilities.jsx
+++ src/components/Abilities.jsx
@@ -8,6 +8,7 @@ export default function Abilities() {
           <h3>next.js</h3>
           <h3>tailwindcss</h3>
           <h3>mongoDB</h3>
+          <h3>mySQL</h3>
           <h3>c#/.net</h3>
           <h3>git</h3>
           <h3>linux</h3>
