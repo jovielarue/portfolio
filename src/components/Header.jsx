c86a523a3f930d2765a788de8990c7ede6d57512
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-10-16 09:24:40 -0600

    update site domain

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,34 @@
 import { Link } from "react-router-dom";
 
 export default function Header(props) {
-
-
   return (
     <>
       <header className="header">
         <nav className="header-nav">
-          <div className="nav-item"><Link to="/">home</Link></div>
-          <div className="nav-item"><Link to="/resume">resume</Link></div>
-          <div className="nav-item"><a href="https://github.com/joshlarue" target={"_blank"} rel={"noreferrer"}>github</a></div>
-          <div className="nav-item"><Link to="/projects">projects</Link></div>
-          <div className="nav-item"><Link to="/contact">contact</Link></div>
+          <div className="nav-item">
+            <Link to="/">home</Link>
+          </div>
+          <div className="nav-item">
+            <Link to="/resume">resume</Link>
+          </div>
+          <div className="nav-item">
+            <a
+              href="https://github.com/jovielarue"
+              target={"_blank"}
+              rel={"noreferrer"}
+            >
+              github
+            </a>
+          </div>
+          <div className="nav-item">
+            <Link to="/projects">projects</Link>
+          </div>
+          <div className="nav-item">
+            <Link to="/contact">contact</Link>
+          </div>
         </nav>
       </header>
     </>
   );
 }
+
